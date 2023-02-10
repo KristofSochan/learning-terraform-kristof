8cac8ba6997020f7db9aaee97a55d2e5bc45828a
KristofSochan <kristof.sochan@gmail.com>
2023-02-09 16:09:19 -0800

    More syntax

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "blog" {
   }
 }
 
-resource "aws_security_grou" "blog" {
+resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow http and https in. Allow everything out."
 
@@ -49,7 +49,7 @@ resource "aws_security_group_rule" "blog_http_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
-  protocol    = -1
+  protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
