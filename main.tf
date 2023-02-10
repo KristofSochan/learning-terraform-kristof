dde0c8ab2227791c7370630cca781832e56aba7e
KristofSochan <kristof.sochan@gmail.com>
2023-02-09 17:31:45 -0800

    Adding http requests

--- main.tf
+++ main.tf
@@ -31,12 +31,12 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "4.17.1"
   name = "blog_new"
 
   vpc_id = data.aws_vpc.default.id
 
-  ingress_rules       = ["all-all"]
+  ingress_rules       = ["http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules       = ["all-all"]
