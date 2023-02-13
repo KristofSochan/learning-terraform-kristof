7c75f12d0dfb9dd422798224050c1b1429e67563
KristofSochan <kristof.sochan@gmail.com>
2023-02-13 09:31:08 -0800

    Auto scaling

--- main.tf
+++ main.tf
@@ -32,18 +32,23 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.7.1"
+  
+  name = "blog"
+  min_size = 1
+  max_size = 2
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
 
-  tags = {
-    Name = "Learning Terraform"
-  }
+  image_id         = data.aws_ami.app_ami.id
+  instance_type    = var.instance_type
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
@@ -88,7 +93,7 @@ module "blog_sg" {
   version = "4.13.0"
   name = "blog_new"
 
-  vpc_id = module.blog_vpc.public_subnets[0]
+  vpc_id = vpc_id = module.vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
